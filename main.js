ad816bcd350615d6d3bb5b32ebddd08f71719d6c
Alex Faustino <alexfaustinosouza@gmail.com>
2023-06-16 22:26:48 -0300

    Atualização

---
+++ main.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('form-maior');
+const errorMessage = 'O número do campo B precisa ser maior';
+const successMessage = 'Formulário validado com sucesso!';
+
+function validaFormulario(){
+    let numberA = document.getElementById('number-a');
+    let numberB = document.getElementById('number-b');
+
+    if(numberB.value > numberA.value){
+        const containerSuccessMessage = document.querySelector('.success-message');
+        containerSuccessMessage.innerHTML = successMessage;
+        containerSuccessMessage.style.display = 'block';
+        numberA.value = '';
+        numberB.value = '';
+    }
+    else{
+        const containerErrorMessage = document.querySelector('.error-message');
+        containerErrorMessage.innerHTML = errorMessage;
+        containerErrorMessage.style.display = 'block';
+    }
+}
+
+form.addEventListener('submit',function (e){
+    e.preventDefault();
+    validaFormulario();
+})
